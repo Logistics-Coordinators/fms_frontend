6b35ba36977fa5e860106f35a237726da63c64f6
Nitish Kumar Bansal <nitishbansal@Nitishs-MacBook-Pro.local>
2023-02-21 20:53:55 +0530

    changes

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -29,6 +29,10 @@ const Settings = () => {
     address: "",
     is_email_reminder_active: true,
     is_sms_reminder_active: true,
+    PM_reminder_email: "",
+    CVOR_reminder_email: "",
+    maintenance_duration: "",
+    safety_duration: "",
   });
 
   const [file, setFile] = useState({
@@ -61,26 +65,33 @@ const Settings = () => {
   formData.append("is_email_reminder_active", data.is_email_reminder_active);
   formData.append("is_sms_reminder_active", data.is_sms_reminder_active);
 
-  for (var pair of formData.entries()) {
-    console.log(pair[0] + ", " + pair[1]);
-  }
+  formData.append("PM_reminder_email", data.PM_reminder_email);
+  formData.append("CVOR_reminder_email", data.CVOR_reminder_email);
+  formData.append("maintenance_duration", data.maintenance_duration);
+  formData.append("safety_duration", data.safety_duration);
 
   const config = {
     headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
   };
 
-  useEffect(() => {
-    Axios.get(
-      `https://lc-backend-v2.herokuapp.com/api/v1/LC/currCompany`,
-      config
-    )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   Axios.get(
+  //     `https://lc-backend-v2.herokuapp.com/api/v1/LC/currCompany`,
+
+  //     config
+  //   )
+  //     .then((response) => {
+  //       setData({
+  //         ...data,
+  //         ["address"]: response.data.company.address,
+  //       });
+  //       console.log(response);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   const handleCompanyUpdate = (e) => {
     e.preventDefault();
 
@@ -99,7 +110,7 @@ const Settings = () => {
       }
     )
       .then((res) => {
-        console.log(res);
+        // console.log(res);
         setMsg("Company info updated!");
         setModal(true);
         setModalColor("green");
@@ -138,7 +149,6 @@ const Settings = () => {
               <label htmlFor="">Change logo:</label>
               <input type="file" name="logo" onChange={fileInputChange} />
             </div>
-            {/* <div className={styles.label_container}></div> */}
             <div className={styles.content_container}>
               <label htmlFor="">Address:</label>
               <textarea
@@ -193,6 +203,42 @@ const Settings = () => {
                 onChange={inputChange}
                 name="country"
                 value={data.country}
+              />
+            </div>
+            <div className={styles.content_container}>
+              <label htmlFor="">PM reminder email:</label>
+              <input
+                type="email"
+                onChange={inputChange}
+                name="PM_reminder_email"
+                value={data.PM_reminder_email}
+              />
+            </div>
+            <div className={styles.content_container}>
+              <label htmlFor="">CVOR reminder email:</label>
+              <input
+                type="email"
+                onChange={inputChange}
+                name="CVOR_reminder_email"
+                value={data.CVOR_reminder_email}
+              />
+            </div>
+            <div className={styles.content_container}>
+              <label htmlFor="">Maintenance duration:</label>
+              <input
+                type="text"
+                onChange={inputChange}
+                name="maintenance_duration"
+                value={data.maintenance_duration}
+              />
+            </div>
+            <div className={styles.content_container}>
+              <label htmlFor="">Safety duration:</label>
+              <input
+                type="text"
+                onChange={inputChange}
+                name="safety_duration"
+                value={data.safety_duration}
               />
             </div>
             <div className={styles.content_container}>
